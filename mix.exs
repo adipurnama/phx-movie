97537f442461b390fb0f7666b987aedfe897a454
Adi Purnama <adi.purnama234@gmail.com>
2021-02-19 13:16:54 +0700

    fix: searchbox error for latest :plug version
    related to https://elixirforum.com/t/missing-clause-of-phoenix-liveview-channel-gather-keys/35226

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Movie.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.5.1"},
-      {:phoenix_live_view, "~> 0.13.1"},
+      {:phoenix_live_view, "~> 0.14.8"},
       {:floki, ">= 0.0.0", only: :test},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
